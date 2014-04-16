71a6e032d20cc8edf87b8b61498d381336379c59
gaoyifan <git@gaoyifan.com>
2014-04-16 19:31:52 +0800

    add safe-rm plugin

---
+++ plugins/safe-rm/saferm.plugin.zsh
@@ -0,0 +1,15 @@
+function rm () {
+  local path
+  for path in "$@"; do
+    # ignore any arguments
+    if [[ "$path" = -* ]]; then :
+    else
+      local dst=${path##*/}
+      # append the time if necessary
+      while [ -e ~/.Trash/"$dst" ]; do
+        dst="$dst "$(date +%H-%M-%S)
+      done
+      /bin/mv "$path" ~/.Trash/"$dst"
+    fi
+  done
+}
