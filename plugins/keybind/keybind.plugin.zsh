83d70126299672a169872fa8380e5473b1af0e38
Yifan Gao <git@gaoyifan.com>
2014-05-18 00:56:34 +0800

    add plugin bindkey

---
+++ plugins/keybind/keybind.plugin.zsh
@@ -0,0 +1,16 @@
+# Description
+# ------------
+#
+# exit plugin will bind a key meta-E to type exit in the current shell
+#
+# ------------------------------------------------------------
+# Authors
+# ---------
+#
+# Yifan Gao <ylgaoyifan@gmail.com>
+#
+# ------------------------------------------------------------
+#
+
+bindkey -s "^[e" "^Cexit\n"
+bindkey -s "^[r" "^Ureboot"
