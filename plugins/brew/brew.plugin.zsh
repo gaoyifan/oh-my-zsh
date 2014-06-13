52437dc98ce408ed00dc6631e46c59770ba79f9c
Yifan Gao <git@gaoyifan.com>
2014-06-13 21:58:19 +0800

    add plugin package-manager to replace apt-get & brew

--- plugins/brew/brew.plugin.zsh
+++
@@ -1,7 +0,0 @@
-if [[ "$os" == "Darwin" ]]; then
-        alias bi='brew install'
-        alias bif='brew info'
-        alias bs='brew search'
-        alias bu='brew update'
-        alias bug='brew upgrade'
-fi
---
+++ plugins/package-manager/package-manager.plugin.zsh
@@ -0,0 +1,32 @@
+# local pm_shell
+
+alias pm="$pm_shell"
+alias pmi='pm install'
+alias pmu='pm update'
+alias pmug='pm upgrade'
+alias pmr='pm remove'
+alias pms='pm search'
+alias pmri='pm reinstall'
+alias pmif='pm info'
+
+if [ `type aptitude >/dev/null 2>&1` ]; then
+    pm_shell='aptitude'
+    if [[ "$USER" != "root" ]]; then
+        pm_shell="sudo $pm_shell"
+    fi
+elif [ `type apt-get >/dev/null 2>&1` ]; then
+    pm_shell='apt-get'
+    if [[ "$USER" != "root" ]]; then
+        pm_shell="sudo $pm_shell"
+    fi
+    alias pm="$pm_shell"
+    alias pmdu='pm dist-upgrade'
+    alias pms='apt-cache search'
+    alias pmif='apt-cache show'
+elif  `type brew >/dev/null 2>&1` ; then
+    pm_shell='brew'
+elif [ `type yum >/dev/null 2>&1` ]; then
+    pm_shell='yum'
+fi
+
+
