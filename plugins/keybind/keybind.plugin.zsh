1bb87d9103c680594eb85df9bcf9998809fcc105
Yifan Gao <git@yfgao.com>
2016-09-25 13:36:07 +0800

    fix sudo keybind

--- plugins/keybind/keybind.plugin.zsh
+++ plugins/keybind/keybind.plugin.zsh
@@ -12,6 +12,22 @@
 # ------------------------------------------------------------
 #
 
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+        LBUFFER="${LBUFFER#$EDITOR }"
+        LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+        LBUFFER="${LBUFFER#sudoedit }"
+        LBUFFER="$EDITOR $LBUFFER"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
 bindkey -s "^[e" "^U^Cexit\n"
 bindkey -s "^[r" "^Ureboot"
-bindkey -s "^[s" "^Asudo ^E"
+bindkey "^[s" sudo-command-line
+
