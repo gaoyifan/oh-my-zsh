f2e49dde8e34dfed7d375e6506b58af9bc9317c7
Yifan Gao <git@yfgao.com>
2018-03-04 23:58:25 +0800

    powerline-shell: force call python2

--- plugins/powerline-shell/powerline-shell.plugin.zsh
+++ plugins/powerline-shell/powerline-shell.plugin.zsh
@@ -1,5 +1,5 @@
 function powerline_precmd() {
-	export PS1="$($ZSH/plugins/powerline-shell/powerline-shell.py $? --shell zsh 2> /dev/null)"
+	export PS1="$(/usr/bin/env $python2_name $ZSH/plugins/powerline-shell/powerline-shell.py $? --shell zsh 2> /dev/null)"
 	export RPS1='%(?..%{[0m%}%{[38;5;124m%}%{[38;5;254m%}%{[48;5;124m%} $?%{[48;5;124m%}) %{[38;5;236m%}%{[38;5;15m%}%{[48;5;236m%} %D{%H:%M:%S} %{[0m%}'
 }
 
@@ -11,6 +11,17 @@ function install_powerline_precmd() {
 	done
 	precmd_functions+=(powerline_precmd)
 }
+
+function get_python2_path() {
+    for ipath in python2 python2.7 python; do
+        if  `type $ipath >/dev/null 2>&1`; then
+            echo $ipath
+            break
+        fi
+    done
+}
+
 if [ "$LC_POWERLINE_FONT" = "true" ]; then
 	install_powerline_precmd
 fi
+export python2_name=$(get_python2_path)
